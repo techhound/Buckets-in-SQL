87ae0ec18d21184bdc47b6af1ddc92d275414d0b
techhound <salesmanager@shopperscove.com>
2020-12-07 21:14:33 -0500

    Add files via upload
    Tables for the article on Creating Buckets in SQL (DataScienceReview.com)

---
+++ Employees.sql
@@ -0,0 +1,18 @@
+CREATE TABLE employees (
+  id mediumint(8) unsigned NOT NULL auto_increment,
+  employeeID mediumint,
+  name varchar(255) default NULL,
+  salary mediumint default NULL,
+  PRIMARY KEY (id)
+) AUTO_INCREMENT=1;
+
+INSERT INTO employees (employeeID,name,salary) VALUES (1,"Jessica",170341),(2,"Mollie",73115),(3,"Lane",95771),(4,"Giacomo",45073),(5,"Thane",145938),(6,"Portia",117753),(7,"Ursa",174973),(8,"Pandora",169572),(9,"Ayanna",104136),(10,"Stephen",54392);
+INSERT INTO employees (employeeID,name,salary) VALUES (11,"Wing",79049),(12,"Mufutau",172036),(13,"Patrick",157453),(14,"Dawn",92392),(15,"Quin",170616),(16,"Connor",107503),(17,"Ahmed",78568),(18,"Hanna",147945),(19,"Vaughan",153714),(20,"Levi",101522);
+INSERT INTO employees (employeeID,name,salary) VALUES (21,"Cameran",117742),(22,"Blossom",164457),(23,"Kiayada",81660),(24,"Len",123098),(25,"Alice",169663),(26,"Anne",123948),(27,"Fletcher",78409),(28,"Venus",158132),(29,"Savannah",158554),(30,"Astra",117425);
+INSERT INTO employees (employeeID,name,salary) VALUES (31,"Rhona",145094),(32,"Blaze",112504),(33,"Geoffrey",125270),(34,"Yetta",70151),(35,"Clare",118337),(36,"Laith",129280),(37,"Neve",57051),(38,"Elaine",131898),(39,"Kane",153295),(40,"Doris",149909);
+INSERT INTO employees (employeeID,name,salary) VALUES (41,"Noble",131322),(42,"Moses",52470),(43,"Nayda",97861),(44,"Barbara",158415),(45,"Remedios",65785),(46,"Elliott",51921),(47,"Clarke",120386),(48,"Baxter",100361),(49,"Roary",51831),(50,"Madonna",179480);
+INSERT INTO employees (employeeID,name,salary) VALUES (51,"Odysseus",131026),(52,"Cherokee",147900),(53,"Reed",117825),(54,"Harrison",141324),(55,"Jasper",116750),(56,"Charity",164321),(57,"Rogan",103184),(58,"Alyssa",137590),(59,"Gregory",97452),(60,"Porter",173330);
+INSERT INTO employees (employeeID,name,salary) VALUES (61,"Hashim",168973),(62,"Linda",49622),(63,"Roanna",65321),(64,"Kathleen",149519),(65,"Hakeem",137421),(66,"Trevor",80502),(67,"Davis",164103),(68,"Boris",105892),(69,"Simone",75042),(70,"Barrett",148876);
+INSERT INTO employees (employeeID,name,salary) VALUES (71,"Brent",93905),(72,"Abra",127961),(73,"Indigo",157153),(74,"Hamish",79760),(75,"Odessa",68781),(76,"Chastity",160761),(77,"Malik",164545),(78,"Faith",99174),(79,"Christian",171052),(80,"Genevieve",178507);
+INSERT INTO employees (employeeID,name,salary) VALUES (81,"Ulla",145565),(82,"Alvin",131358),(83,"Maggy",151350),(84,"Kenneth",172542),(85,"Sierra",93831),(86,"Bree",179735),(87,"Mufutau",72885),(88,"Ray",63355),(89,"Hollee",46816),(90,"Damon",110167);
+INSERT INTO employees (employeeID,name,salary) VALUES (91,"Xenos",50927),(92,"Clio",85737),(93,"Eaton",119190),(94,"Judah",50145),(95,"Echo",143119),(96,"Alika",66222),(97,"Keegan",67044),(98,"Bevis",141134),(99,"Yoshio",50523),(100,"Shelley",115675);
